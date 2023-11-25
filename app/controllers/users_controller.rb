0b2ee3e692c4108ebac8a6e0834ceea696d9d7f2
Juan Carlos Saieh <juanca_saieh@hotmail.com>
2023-11-24 20:10:52 -0500

    added user controller stub

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,24 +5,55 @@ class UsersController < ApplicationController
 		@user = User.all
 	end
 
-	def new
-		#@user = User.new
-	end
-
 	def create
+		conn = Faraday.new(url: 'http://192.168.0.101:3001')
 		
+		response = conn.post('/users') do |req|
+  			req.headers['Content-Type'] = 'application/json'
+  			req.body = JSON.generate(user_params)
+		end
+		if response.status == 200
+			# redirect to show
+		else
+			render status: 422
 	end
 
 	def show
-		
+		response = Faraday.get('http://192.168.0.101:3001/users/'
+								+ params[:docType] + '/' + params[:docNum])
+		if response.status = 200
+			render json: response.body
+		else
+			render status: :not_found
 	end
 
 	def update
-
+		conn = Faraday.new(url: 'http://192.168.0.101:3001')
+		response = conn.patch('users/' + params[:id]) do |req|
+		  	req.headers['Content-Type'] = 'application/json'
+		  	req.body = JSON.generate(user_params)
+		end
+		if response.status = 200
+			# redirect to show
+		else
+			render status: 422
+		end
 	end
 
-	def delete
+	def destroy
+		conn = Faraday.new(url: 'http://192.168.0.101:3001')
+		response = conn.delete('/users/' + params[:id])
+		if response.status = 200
+			# redirect to index or consult view
+		else
+			render status: :not_found
+		end
+	end
 
+	def user_params
+		params.require(:user).permit(:docType,
+				:docNum, :firstName, :secondName, :lastName,
+				:dateBirth, :gender, :email, :phoneNumber)
 	end
 
 end
